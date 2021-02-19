1a4815b2ec217de42665032dd031535f833dacfc
jaymelegrito <melegrito.john@gmail.com>
2021-02-19 15:10:05 +0800

    updated PhoneBillCalculatorRedo

--- HelloJava/src/chapter6/PhoneBillCalculatorRedo.java
+++ HelloJava/src/chapter6/PhoneBillCalculatorRedo.java
@@ -1,8 +1,20 @@
 package chapter6;
 
+import java.util.Scanner;
+
 public class PhoneBillCalculatorRedo{
     public static void main(String [] args){
-        PhoneBill phoneBill = new PhoneBill(123890, 1450.75, 10, 6.50);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter subscriber ID:");
+        int subscriberId = scanner.nextInt();
+
+        System.out.println("Enter overage minutes:");
+        double overMinutes = scanner.nextDouble();
+        
+        PhoneBill phoneBill = new PhoneBill(subscriberId, 1450.75, 10, overMinutes);
         phoneBill.printItemizedBill();
+
+        scanner.close();
     }
 }

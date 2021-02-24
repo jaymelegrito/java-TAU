567b75090b0fff803995b33265f42a5f45cc8a15
jaymelegrito <melegrito.john@gmail.com>
2021-02-24 21:37:31 +0800

    Created chapter 8 exercises

---
+++ HelloJava/src/chapter8/Strings.java
@@ -0,0 +1,11 @@
+package chapter8;
+
+public class Strings {
+
+    public static void main(String[] args){
+
+        String str = "abc";
+        System.out.println(str.hashCode());
+    }
+    
+}

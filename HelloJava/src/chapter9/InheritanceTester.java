018b543752413a721b1ea40da5aa6b9b6ff4dab4
jaymelegrito <melegrito.john@gmail.com>
2021-02-26 22:24:25 +0800

    created chapter9 - Inheritance

---
+++ HelloJava/src/chapter9/InheritanceTester.java
@@ -0,0 +1,14 @@
+package chapter9;
+
+public class InheritanceTester {
+
+    public static void main(String[] args){
+
+        Person person = new Person();
+        Employee employee = new Employee();
+
+        person.getAge();
+        employee.getEmployeeId();
+    }
+    
+}
